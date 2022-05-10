57783d66ec5ba3cdefbb3e4b81dc45b7ae975391
mandeepsng <1mandeep2021@gmail.com>
2022-05-10 10:03:14 +0530

    author details update

--- data/authors/default.md
+++ data/authors/default.md
@@ -1,15 +1,15 @@
 ---
-name: Tails Azimuth
+name: Mandeep Singh
 avatar: /static/images/avatar.png
-occupation: Professor of Atmospheric Science
-company: Stanford University
-email: address@yoursite.com
-twitter: https://twitter.com/Twitter
+occupation: Nextjs and Laravel developer
+company: Rvs Media
+email: yoblogger7@gmail.com
+twitter: https://twitter.com/mandeep_manuu
 linkedin: https://www.linkedin.com
 github: https://github.com
 ---
 
-Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His research interests includes complexity modelling of tailwinds, headwinds and crosswinds.
+Mandeep Singh is a Nextjs and Laravel developer .
 
 He leads the clean energy group which develops 3D air pollution-climate models, writes differential equation solvers, and manufactures titanium plated air ballons. In his free time he bakes raspberry pi.
 
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  author: 'Mandeep Singh',
+  headerTitle: 'Mandeep Singh Dev',
+  description: 'Mandeep Singh Blog - Nextjs, Reactjs and Laravel',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -10,9 +10,9 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
+  email: 'yoblogger7@gmail.com',
   github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
+  twitter: 'https://twitter.com/mandeep_manuu',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
